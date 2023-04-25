e65d6e98e2ed293da6c777676435dbd595bef789
lipuyue <1054716663@qq.com>
2023-04-25 16:08:55 +0800

    import

---
+++ long_test.py
@@ -0,0 +1,9 @@
+
+from ppasr.predict import PPASRPredictor
+
+predictor = PPASRPredictor(model_tag='conformer_streaming_fbank_wenetspeech')
+
+wav_path = 'dataset/test_long.wav'
+result = predictor.predict_long(audio_data=wav_path, use_pun=False)
+score, text = result['score'], result['text']
+print(f"识别结果: {text}, 得分: {score}")
---
+++ short_test.py
@@ -0,0 +1,8 @@
+from ppasr.predict import PPASRPredictor
+
+predictor = PPASRPredictor(model_tag='conformer_streaming_fbank_wenetspeech')
+
+wav_path = 'dataset/test.wav'
+result = predictor.predict(audio_data=wav_path, use_pun=False)
+score, text = result['score'], result['text']
+print(f"识别结果: {text}, 得分: {int(score)}")
